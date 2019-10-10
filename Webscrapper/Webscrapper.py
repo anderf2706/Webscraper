b3216371b9e479479efa10ba3d46b5e1f3870d13
anderf2706 <anderf2706@gmail.com>
2019-10-10 15:57:41 +0200

    Initial commit

---
+++ Webscrapper/Webscrapper.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+result = requests.get("https://www.google.com/")
+print(result.status_code)
+print(result.headers)
+src = result.content
+print(src)
