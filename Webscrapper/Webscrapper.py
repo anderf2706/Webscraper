317d203f29a05d364985099a8569caccdef83fcf
anderf2706 <anderf2706@gmail.com>
2019-10-11 00:41:23 +0200

    1

--- Webscrapper/Webscrapper.py
+++ Webscrapper/Webscrapper.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-result = requests.get("https://www.google.com/")
+result = requests.get("http://ethans_fake_twitter_site.surge.sh/")
 print(result.status_code)
 print(result.headers)
 src = result.content
 print(src)
+
+soup = BeautifulSoup(src, "html.parser")
+tweet = soup.findAll('p', attrs={"class": "content"})
+print(tweet)
