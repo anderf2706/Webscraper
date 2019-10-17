b924713d74885d8d49b2924eba4d084e7b189edb
anderf2706 <anderf2706@gmail.com>
2019-10-18 00:46:32 +0200

    update

--- Webscrapper/Webscrapper.py
+++ Webscrapper/Webscrapper.py
@@ -11,18 +11,18 @@ content = BeautifulSoup(src, "html.parser")
 tweetArr = []
 for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
     tweetobject = {
-        "author" : tweet.find("h2" , attrs={"class" : "author"}.text.encode('utf-8')),
-        "tweet" : tweet.find("p", attrs={"class": "content"}.text.encode("utf-8")),
-        "date" : tweet.find("h5", attrs={"class": "dateTime"}.text.encode("utf-8")),
-        "likes" : tweet.find("p", attrs={"class": "Likes"}.text.encode("utf-8")),
-        "shares" : tweet.find("p", attrs={"class" : "Shares"}.text.encode("utf-8"))
+        "author" : tweet.find("h2" , attrs={"class" : "author"}).get_text(),
+        "tweet" : tweet.find("p", attrs={"class": "content"}).get_text(),
+        "date" : tweet.find("h5", attrs={"class": "dateTime"}).get_text(),
+        "likes" : tweet.find("p", attrs={"class": "Likes"}),
+        "shares" : tweet.find("p", attrs={"class" : "Shares"})
     }
-    tweetArr.append(tweetobject)
+    print(tweetobject)
+
 with open("twitterData.json", "w") as outfile:
     json.dump(tweetArr, outfile)
 
 
-    #print(tweet.text.encode('utf-8'))
 print("done")
 
 
