f9afda362c4060b296228d3f7597c5db1018a465
anderf2706 <anderf2706@gmail.com>
2019-10-25 21:12:19 +0200

    update

---
+++ Redditscrapper/Redditscrapper.py
@@ -0,0 +1,16 @@
+#! python3
+import pandas as pd
+import praw
+import datetime as dt
+
+reddit = praw.Reddit(client_id='ZMYsLBXNwHSteQ',
+                     client_secret='7ylJljQjBWxgZThkDp_dtW194pE',
+                     user_agent='scraper',
+                     username='anderf2706',
+                     password='3280TjodalynG')
+
+subreddit = reddit.subreddit('stocks')
+
+top_subreddit = subreddit.hot(limit=1000)
+for post in top_subreddit:
+    print(post.title, "   :   ",post.id)
