d3311b85c40319c2c99830f5bf419ff5af3cda0f
anderf2706 <anderf2706@gmail.com>
2019-10-26 02:38:10 +0200

    update

--- Webscrapper/Webscrapper.py
+++ Webscrapper/Webscrapper.py
@@ -1,38 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-import praw
+from selenium import webdriver
+import time
 
-redditBot = praw.Reddit()
 
-result = requests.get("https://www.reddit.com/r/stocks/")
-print(result.status_code)
-print(result.headers)
+selskap = input("selskap:")
+for i in range(0, len(selskap)):
+    if selskap[i] == " ":
+        selskap = selskap.replace(" ", "%20")
+
+result = requests.get("https://www.proff.no/bransjes%C3%B8k?q=" + selskap)
+#print(result.status_code)
+#print(result.headers)
 src = result.content
 
-
-
 content = BeautifulSoup(src, "html.parser")
-tweetArr = []
-for reddit in content.findAll('div', attrs={"style"}):
-    redditobject = {
 
-        """
-        "author" : tweet.find("h2" , attrs={"class" : "author"}).get_text(),
-        "tweet" : tweet.find("p", attrs={"class": "content"}).get_text(),
-        "date" : tweet.find("h5", attrs={"class": "dateTime"}).get_text(),
-        "likes" : tweet.find("p", attrs={"class": "Likes"}),
-        "shares" : tweet.find("p", attrs={"class" : "Shares"})
-        """
-        "content": reddit.find("h3", attrs={"_eYtD2XCVieq6emjKBH3m"}).get_text()
-    }
-    print(redditobject)
-    #tweetArr.append(redditobject)
+for hit in content.findAll(attrs={"class": "addax addax-cs_hl_hit_company_name_click"}):
+    strings = str(hit).split('href=')
+    dummiestrings = strings[1]
+    dummiestrings = dummiestrings.split('>')
+    dummiestrings = dummiestrings[0].replace('"', "")
+    break
+print(dummiestrings)
+
+result_new = requests.get("https://www.proff.no" + dummiestrings)
+driver = webdriver.Chrome()
+driver.get("https://www.proff.no" + dummiestrings)
+
 
 with open("twitterData.json", "w") as outfile:
     json.dump(tweetArr, outfile)
 
-
 print("done")
-
 
