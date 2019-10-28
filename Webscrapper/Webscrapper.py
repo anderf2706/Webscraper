8c1cc0545b7c9551673b227a8925a7978e7b1e86
anderf2706 <anderf2706@gmail.com>
2019-10-28 23:49:49 +0100

    update

--- Webscrapper/Webscrapper.py
+++ Webscrapper/Webscrapper.py
@@ -22,12 +22,46 @@ for hit in content.findAll(attrs={"class": "addax addax-cs_hl_hit_company_name_c
     dummiestrings = strings[1]
     dummiestrings = dummiestrings.split('>')
     dummiestrings = dummiestrings[0].replace('"', "")
+    dummiestrings = dummiestrings.replace('selskap', 'nokkeltall')
     break
 print(dummiestrings)
 
-result_new = requests.get("https://www.proff.no" + dummiestrings)
+result_nokkeltall = requests.get("https://www.proff.no" + dummiestrings)
+src_nokkeltall = result_nokkeltall.content
+content_nokkel = BeautifulSoup(src_nokkeltall, "html.parser")
+
+ar = 2018
+Totalrentabilitet = {}
+for element in content_nokkel.find_all('tr', attrs={'data-chart-title': 'Totalrentabilitet i %'}):
+    Totalrentabilitet[ar] = element.find('td').get_text()
+    ar -= 1
+    print(ar)
+
+print(Totalrentabilitet.keys())
+print(Totalrentabilitet.values())
+
+ar = 2018
+Resultat_Drift = {}
+for element in content_nokkel.find_all('tr', attrs={'data-chart-title': 'Resultat av driften i %'}):
+    Resultat_Drift[ar] = element.find('td').get_text()
+    ar -= 1
+    print(ar)
+
+print(Resultat_Drift.keys())
+print(Resultat_Drift.values())
+
+
+Egenkapitalens_rentabilitet_preSkatt = []
+Likviditetsgrad = []
+Egenkapitalandel = []
+Gjeldsgrad = []
+
 driver = webdriver.Chrome()
 driver.get("https://www.proff.no" + dummiestrings)
+
+
+
+
 
 """""
 
