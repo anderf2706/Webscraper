e149388f2dafde7b8d147747295903cca0b660aa
anderf2706 <anderf2706@gmail.com>
2019-11-16 15:03:31 +0100

    update on gather_all

--- Webscrapper/keyvalues.py
+++ Webscrapper/keyvalues.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+
 def generate_content_nokkel(company):
     global content_nokkel
     try:
@@ -16,30 +17,75 @@ def generate_content_nokkel(company):
 
 
 def gather_all():
+    Basic = {}
+    Deluted = {}
+    Total_revenue = {}
+    Cost_of_revenue = {}
+    Gross_profit = {}
+    Research_development = {}
+    Selling_general_Administrativ ={}
+    Total_operating_expenses = {}
+    Operating_income_or_loss = {}
+    Interest_expenses = {}
+    Total_other_income_or_expenses_net = {}
+    Income_before_tax = {}
+    Income_tax_expenses = {}
+    Income_from_Continuing_Operations = {}
+
+
     indexes = ['D(tbr) fi-row Bgc($hoverBgColor):h',
                ]
-    
-    for i in 1:len(indexes)
-    ar = 2018
-    totalrentabilitet = {}
-    i = 0
-    for element in content_nokkel.find('div', attrs={'class': 'D(tbr) fi-row Bgc($hoverBgColor):h'}):
-        nameofvalue = str
-        for first_block in element:
-            for value in first_block:
-                if i == 0:
-                    first = str(value).split('>')[1]
-                    nameofvalue = first.split('<')[0]
-                    print(nameofvalue)
-                    i = 1
-                else:
-                    print(str(value))
+
+    for i in range(0, len(indexes)):
+        ar = 2018
+        for element in content_nokkel.findAll('div', attrs={'class': 'D(tbr) fi-row Bgc($hoverBgColor):h'}):
+            nameofvalue = str
+            for first_block in element:
+                for value in first_block:
+                    valuestr = str(value)
+                    ar = 2018
+                    i = 0
+                    #if not value.__contains__("div") and not value.__contains__("span") and i < 6:
+                    #    valuestr.replace(".", ",")
+                    #    try:
+                    #        Basic[ar] = float(value)
+                    #    except ValueError:
+                    #        Basic[ar] = None
+                    #    i += 1
+                    #    ar -= 1
+                    #if not value.__contains__("div") and not value.__contains__("span") and i > 6:
+                    #    valuestr.replace(".", ",")
+                    #    try:
+                    #        Deluted[ar] = float(value)
+                    #    except ValueError:
+                    #        Deluted[ar] = None
+                    #    i += 1
+                    #    ar -= 1
+                    #
+                    for last2 in value:
+                        last2 = str(last2)
+                        if last2.__contains__('span'):
+                            first = str(last2).split('>')[1]
+                            nameofvalue = first.split('<')[0]
+                            print("navn " + nameofvalue)
+                        if not last2.__contains__('button') and not last2.__contains__('span')\
+                                and not nameofvalue == 'Basic' and not nameofvalue == 'Diluted':
+                            last2.replace(',', '')
+                            print("number" + str(last2))
+                        if not nameofvalue == 'Basic' and not nameofvalue == 'Diluted':
 
 
 
-
-
-
+                    #if i == 0:
+                     #   first = str(value).split('>')[1]
+                      #  nameofvalue = first.split('<')[0]
+                       # print(nameofvalue)
+                        #i = 1
+                    #else:
+                    #print(str(value))
+                    #print("hqllq")
+    print(Basic)
+    print(Deluted)
 
 generate_content_nokkel('AAPL')
 gather_all()
