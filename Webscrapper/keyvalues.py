19574d4b85a832cc47e670aeebdc53c9c9639b69
anderf2706 <anderf2706@gmail.com>
2019-11-17 23:23:58 +0100

    update on gather_all

--- Webscrapper/keyvalues.py
+++ Webscrapper/keyvalues.py
@@ -15,15 +15,15 @@ def generate_content_nokkel(company):
     except:
         print('no key values available')
 
-
-def gather_all():
+def make_lists():
+    lists = []
     Basic = {}
     Deluted = {}
     Total_revenue = {}
     Cost_of_revenue = {}
     Gross_profit = {}
     Research_development = {}
-    Selling_general_Administrativ ={}
+    Selling_general_Administrativ = {}
     Total_operating_expenses = {}
     Operating_income_or_loss = {}
     Interest_expenses = {}
@@ -31,8 +31,27 @@ def gather_all():
     Income_before_tax = {}
     Income_tax_expenses = {}
     Income_from_Continuing_Operations = {}
+    lists.append(Basic)
+    lists.append(Deluted)
+    lists.append(Total_revenue)
+    lists.append(Cost_of_revenue)
+    lists.append(Gross_profit)
+    lists.append(Research_development)
+    lists.append(Selling_general_Administrativ)
+    lists.append(Total_operating_expenses)
+    lists.append(Operating_income_or_loss)
+    lists.append(Interest_expenses)
+    lists.append(Total_other_income_or_expenses_net)
+    lists.append(Income_before_tax)
+    lists.append(Income_tax_expenses)
+    lists.append(Income_from_Continuing_Operations)
+    return lists
 
 
+def gather_all():
+
+    list_of_dict = make_lists()
+    print(list_of_dict)
     indexes = ['D(tbr) fi-row Bgc($hoverBgColor):h',
                ]
 
@@ -45,23 +64,25 @@ def gather_all():
                     valuestr = str(value)
                     ar = 2018
                     i = 0
-                    #if not value.__contains__("div") and not value.__contains__("span") and i < 6:
-                    #    valuestr.replace(".", ",")
-                    #    try:
-                    #        Basic[ar] = float(value)
-                    #    except ValueError:
-                    #        Basic[ar] = None
-                    #    i += 1
-                    #    ar -= 1
-                    #if not value.__contains__("div") and not value.__contains__("span") and i > 6:
-                    #    valuestr.replace(".", ",")
-                    #    try:
-                    #        Deluted[ar] = float(value)
-                    #    except ValueError:
-                    #        Deluted[ar] = None
-                    #    i += 1
-                    #    ar -= 1
-                    #
+                    if not(valuestr.__contains__("div") or valuestr.__contains__("span")) and i < 6:
+                        print(value)
+                        #valuestr.replace(".", ",")
+                        #try:
+                        #    Basic[ar] = float(value)
+                        #except ValueError:
+                        #    Basic[ar] = None
+                        #i += 1
+                        #ar -= 1
+                    if not(valuestr.__contains__("div") or valuestr.__contains__("span")) and i > 6:
+                        print(value)
+                        #valuestr.replace(".", ",")
+                        #try:
+                        #    Deluted[ar] = float(value)
+                        #except ValueError:
+                        #    Deluted[ar] = None
+                        #i += 1
+                        #ar -= 1
+
                     for last2 in value:
                         last2 = str(last2)
                         if last2.__contains__('span'):
@@ -70,9 +91,14 @@ def gather_all():
                             print("navn " + nameofvalue)
                         if not last2.__contains__('button') and not last2.__contains__('span')\
                                 and not nameofvalue == 'Basic' and not nameofvalue == 'Diluted':
-                            last2.replace(',', '')
-                            print("number" + str(last2))
-                        if not nameofvalue == 'Basic' and not nameofvalue == 'Diluted':
+                            number = last2.replace(',', '')
+                            print("number", number)
+                            for dict in list_of_dict:
+                                nametosearch = nameofvalue.replace(' ', '_')
+                                #if nametosearch == dict
+                        #if nameofvalue == 'Basic' or nameofvalue == 'Diluted':
+
+
 
 
 
@@ -84,8 +110,7 @@ def gather_all():
                     #else:
                     #print(str(value))
                     #print("hqllq")
-    print(Basic)
-    print(Deluted)
+
 
 generate_content_nokkel('AAPL')
 gather_all()
